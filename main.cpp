ed9eaf3af714a6c51abd92aedf3b2f5d35317469
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-02-21 00:38:34 -0800

    testing average in main and compiling

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include "getSum.h"
+#include "getAverage.h"
 using namespace std;
 
 int main()
 {
 	vector<int> test = {1,1,1,1,1,1,1,1,1,1};
-	cout<<getSum(test);
-	
+	cout<<getSum(test)<<endl;
+	cout<<getAverage(test)<<endl;
+
 	return 0;
 }
