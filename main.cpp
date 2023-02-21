4c61512bfc28e8cd375e412f266fd0b88c22169d
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-02-21 01:00:19 -0800

    fixing conflicts after rebase

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include "getSum.h"
 #include "getAverage.h"
+#include "getMin.h"
+
 using namespace std;
 
 int main()
 {
-	vector<int> test = {1,1,1,1,1,1,1,1,1,1};
+	vector<int> test = {1,1,1,1,1,1,1,1,5,10};
 	cout<<getSum(test)<<endl;
 	cout<<getAverage(test)<<endl;
+	cout<<getMin(test)<<endl;
 
 	return 0;
 }
